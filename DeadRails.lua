c093f0ea12616c8313aada4441c525c9e8f953bd
Anonymous <hmmm99584@gmail.com>
2025-07-12 10:41:30 +0530

    Update DeadRails.lua

--- DeadRails.lua
+++ DeadRails.lua
@@ -1,5 +1,5 @@
 -- 🌟 Load Rayfield
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local realKey = "Sweetie Fox"
 
